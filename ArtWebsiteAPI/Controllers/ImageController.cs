ef8dd545a1919fde5232035ee3e976e80c4168a8
josehirshman <josehirshman@jhengineerartist.com>
2023-08-28 22:28:19 -0700

    implemented GET and DELETE

--- ArtWebsiteAPI/Controllers/ImageController.cs
+++ ArtWebsiteAPI/Controllers/ImageController.cs
@@ -2,7 +2,7 @@ using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using ArtWebsiteDataAccess.Models;
 using ArtWebsiteDataAccess;
-using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtWebsiteAPI.Controllers;
 [Route("api/[controller]")]
@@ -20,12 +20,61 @@ public class ImageController : Controller
         this.blobServiceClient = blobServiceClient; // Assign the blob service client instance
     }
 
-    // GET api/image/upload
-    [HttpGet("upload")]
-    public IActionResult Upload()
+    [HttpGet]
+    public async Task<IActionResult> Get()
     {
-        // Return the view
-        return View();
+        // Get all the image entities from the database using the DbContext class
+        var images = await dbContext.Images.ToListAsync();
+        logger.LogInformation("Retrieved {Count} images from the database", images.Count);
+
+        // Return the image array as a response
+        return Ok(images);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            // Find the image entity by id using the DbContext class
+            var image = await dbContext.Images.FindAsync(id);
+            if (image == null)
+            {
+                logger.LogWarning("Image not found with id: {id}", id);
+                return NotFound("Image not found");
+            }
+
+            // Get a reference to the container using the blob service client
+            logger.LogDebug("Getting reference to container");
+            var container = blobServiceClient.GetBlobContainerClient("apiimages");
+
+            // Get the blob name from the image URL
+            var blobname = Path.GetFileName(image.Url);
+
+            // Delete the blob from the container
+            var response = await container.DeleteBlobAsync(blobname);
+            logger.LogInformation("Blob {blobname} deleted successfully with {response}", blobname, response);
+
+            // Delete the image entity from the database using the DbContext class
+            dbContext.Images.Remove(image);
+            await dbContext.SaveChangesAsync();
+            logger.LogInformation("Image deleted successfully with ID {Id}", image.Id);
+
+            return Ok($"Image with ID {id} deleted successfully");
+        }
+        catch (Azure.RequestFailedException ex)
+        {
+            // Log or display the error details
+            logger.LogError(ex, "Blob client creation failed: {ErrorCode} {Message} {Status}", ex.ErrorCode, ex.Message, ex.Status);
+            return StatusCode(500, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Image deletion failure: {ErrorCode} {Message} {Status}", ex.Message);
+            // Handle any errors that may occur
+            return StatusCode(500, ex.Message);
+        }
+
     }
 
     // POST api/image
